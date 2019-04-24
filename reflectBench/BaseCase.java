52854f7d52e027e6d09034e007aa1a21e14da994
SeanCoek <maze.zxl@gmail.com>
2019-04-24 18:06:11 +0800

    upload Violist result

--- reflectBench/BaseCase.java
+++
@@ -1,9 +0,0 @@
-public class BaseCase {
-
-    public void doReflect(String className) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        Class c = Class.forName(className);
-        Object o = c.newInstance();
-        System.out.println(this.getClass().toString());
-        System.out.println(o.toString());
-    }
-}
